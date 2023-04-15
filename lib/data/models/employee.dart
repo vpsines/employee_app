0dfca386f04deb858b7a2ff318c606af3635edad
vyshnav.sasidharan <sasip6028@gmail.com>
2023-04-16 01:48:12 +0530

    refactor: add isDeleted to employee model

--- lib/data/models/employee.dart
+++ lib/data/models/employee.dart
@@ -3,11 +3,13 @@ class Employee {
   final String role;
   final DateTime fromDate;
   final DateTime? toDate;
+  final bool isDeleted;
 
   Employee({
     required this.name,
     required this.role,
     required this.fromDate,
     this.toDate,
+    this.isDeleted = false
   });
 }
