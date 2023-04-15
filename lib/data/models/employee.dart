bec2461c86ee870f3419eccd01206a05b7d05085
vyshnav.sasidharan <sasip6028@gmail.com>
2023-04-15 20:40:34 +0530

    feat: create employee model

--- lib/data/models/employee.dart
+++ lib/data/models/employee.dart
@@ -1,1 +1,9 @@
-// employee model
+class Employee{
+  final String name;
+  final String role;
+  final DateTime fromDate;
+  final DateTime? toDate;
+
+  Employee({required this.name,required this.role, required this.fromDate,this.toDate});
+
+}
