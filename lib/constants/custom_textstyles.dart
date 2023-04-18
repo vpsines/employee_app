ab3e4225b9cdb47edd54880d224e3b89c39c1733
vyshnav.sasidharan <sasip6028@gmail.com>
2023-04-18 08:48:51 +0530

    style: update style constants

--- lib/constants/custom_textstyles.dart
+++ lib/constants/custom_textstyles.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final kAppBarTitleTextStyle = GoogleFonts.roboto(
   fontSize: 18,
-  fontWeight: FontWeight.w500
+  fontWeight: FontWeight.w500,
+  color: Colors.white
 );
 
 final kBodyTextStyle = GoogleFonts.roboto(
@@ -27,4 +28,22 @@ final kListItemSubTitleTextStyle = GoogleFonts.roboto(
   fontSize: 14,
   fontWeight: FontWeight.w400,
   color: const Color(0xFF949C9E)
+);
+
+final kInputTextStyle = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: const Color(0xFF323238)
+);
+
+final kHintTextStyle = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: const Color(0xFF949C9E)
+);
+
+final kDropDownItemTextStyle = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: const Color(0xFF323238)
 );
